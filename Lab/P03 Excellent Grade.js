ef96e00f6b7467f82a31089131a138d1b7806208
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-06-22 23:45:10 +0300

    This is the solution for the problem "Excellent Grade"

---
+++ Lab/P03 Excellent Grade.js
@@ -0,0 +1,10 @@
+function evaluation(number) {
+    if (number >= 5.50) {
+        console.log('Excellent')
+    } else {
+        console.log('Not excellent')
+    }
+}
+
+evaluation(5.49);
+evaluation(5.50)
