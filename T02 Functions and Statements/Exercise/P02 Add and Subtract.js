63be964b1e7b1021d296a708c386af7d85c18740
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-06-27 23:17:07 +0300

    This is the solution of the problem "Add and Subtract"

---
+++ T02 Functions and Statements/Exercise/P02 Add and Subtract.js
@@ -0,0 +1,14 @@
+function addAndSubtract(firstNumber, secondNumber, thirdNumber) {
+    function sum(a, b) {
+        return a + b
+    }
+    
+    function subtract(a, b) {
+        return a - b;
+    }
+    
+   return  subtract(sum(firstNumber, secondNumber), thirdNumber)
+}
+
+
+
