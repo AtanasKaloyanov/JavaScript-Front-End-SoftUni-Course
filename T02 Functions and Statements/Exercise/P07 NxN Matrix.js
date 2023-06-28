98c5e7ac3871694b634641fcda7c49d2ddc3d1c1
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-06-28 18:13:11 +0300

    This is the solution of the problem "NxN Problem"

---
+++ T02 Functions and Statements/Exercise/P07 NxN Matrix.js
@@ -0,0 +1,12 @@
+function printMatrix(number) {
+
+    let array = [];
+    for (i = 0; i < number; i++) {
+        array.push(number)
+    }
+
+    for (i = 0; i < number; i++) {
+        console.log(array.join(' '))
+    }
+}
+
