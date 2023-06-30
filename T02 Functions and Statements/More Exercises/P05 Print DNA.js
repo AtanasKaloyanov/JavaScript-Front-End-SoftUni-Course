91206e8c2fc3852513e150d0f87f680a6fdb4692
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-07-01 01:09:01 +0300

    This is the solution for the problem "Print DNA"

--- T02 Functions and Statements/More Exercises/P05 Print DNA.js
+++ T02 Functions and Statements/More Exercises/P05 Print DNA.js
@@ -14,77 +14,80 @@ function dnaPrinting(number) {
       if (i == 1) {
         row = '**AT**'
         toTheEdges = true
-        console.log(row)
       } else {
-        rowCreating(row, firstLetter, secondLetter, toTheEdges)
-        console.log(row)
+        row = rowCreating(row, firstLetter, secondLetter, toTheEdges)[0]
+        toTheEdges = rowCreating(row, firstLetter, secondLetter, toTheEdges)[1]
       }
     } else if (i % 5 == 2) {
       firstLetter = pattern.charAt(2)
       secondLetter = pattern.charAt(3)
 
-      rowCreating(row, firstLetter, secondLetter, toTheEdges)
-      console.log(row)
+      row = rowCreating(row, firstLetter, secondLetter, toTheEdges)[0]
+      toTheEdges = rowCreating(row, firstLetter, secondLetter, toTheEdges)[1]
     } else if (i % 5 == 3) {
       firstLetter = pattern.charAt(4)
       secondLetter = pattern.charAt(5)
 
-      rowCreating(row, firstLetter, secondLetter, toTheEdges)
+     row = rowCreating(row, firstLetter, secondLetter, toTheEdges)[0]
+     toTheEdges = rowCreating(row, firstLetter, secondLetter, toTheEdges)[1]
     } else if (i % 5 == 4) {
       firstLetter = pattern.charAt(6)
       secondLetter = pattern.charAt(7)
 
-      rowCreating(row, firstLetter, secondLetter, toTheEdges)
-      console.log(row)
+     row = rowCreating(row, firstLetter, secondLetter, toTheEdges)[0]
+     toTheEdges = rowCreating(row, firstLetter, secondLetter, toTheEdges)[1]
+
     } else if (i % 5 == 0) {
       firstLetter = pattern.charAt(8)
       secondLetter = pattern.charAt(9)
 
-      rowCreating(row, firstLetter, secondLetter, toTheEdges)
-      console.log(row)
+      row = rowCreating(row, firstLetter, secondLetter, toTheEdges)[0]
+      toTheEdges = rowCreating(row, firstLetter, secondLetter, toTheEdges)[1]
     }
+
+    console.log(row)
   }
-}
 
-dnaPrinting(4)
-
-function rowCreating(row, firstLetter, secondLetter, toTheEdges) {
-  let rowFirstChar = row.charAt(0)
-  let rowSecondChar = row.charAt(1)
-  let rowThirdChar = row.charAt(2)
-  let rowFourthChar = row.charAt(3)
-  let rowFifthChar = row.charAt(4)
-  let rowSixthChar = row.charAt(5)
-
-  if (rowFirstChar != '*' && rowSixthChar != '*') {
-    row = '*' + row.slice(1)
-    row = row.slice(0, row.length - 1) + '*'
-    row = row.slice(0, 1) + firstLetter + row.slice(1)
-    row = row.slice(0, 4) + secondLetter + row.slice(4)
-    toTheEdges = false
-  } else if (rowThirdChar != '*' && rowFourthChar != '*') {
-    row = row.slice(0, 2) + '*' + row.slice(2)
-    row = row.slice(0, 3) + '*' + row.slice(3)
-    row = row.slice(0, 1) + firstLetter + row.slice(1)
-    row = row.slice(0, 4) + secondLetter + row.slice(4)
-    toTheEdges = true
-  } else if (rowSecondChar != '*' && rowFifthChar != '*') {
-    rowSecondChar = '*'
-    rowFourthChar = '*'
-
-    if (toTheEdges) {
-      rowFirstChar = firstLetter
-      rowSixthChar = secondLetter
+  function rowCreating(row, firstLetter, secondLetter, toTheEdges) {
+    let rowFirstChar = row.charAt(0)
+    let rowSecondChar = row.charAt(1)
+    let rowThirdChar = row.charAt(2)
+    let rowFourthChar = row.charAt(3)
+    let rowFifthChar = row.charAt(4)
+    let rowSixthChar = row.charAt(5)
+    
+    if (rowFirstChar != '*' && rowSixthChar != '-' ) {
+      row = '*' + row.slice(1)
+      row = row.slice(0, 5) + '*'
+      row = row.slice(0, 1) + firstLetter + row.slice(2)
+      row = row.slice(0, 4) + secondLetter + row.slice(5)
       toTheEdges = false
-    } else {
-      rowThirdChar = firstLetter
-      rowFourthChar = secondLetter
+    } else if (rowThirdChar != '*' && rowFourthChar != '-') {
+      row = row.slice(0, 2) + '-' + row.slice(3)
+      row = row.slice(0, 3) + '-' + row.slice(4)
+      row = row.slice(0, 1) + firstLetter + row.slice(2)
+      row = row.slice(0, 4) + secondLetter + row.slice(5)
       toTheEdges = true
+    } else if (rowSecondChar != '*' && rowFifthChar != '-') {
+  
+      if (toTheEdges) {
+        row = row.slice(0, 1) + '-' + row.slice(2)
+        row = row.slice(0, 4) + '-' + row.slice(5)
+        row = firstLetter + row.slice(1)
+        row = row.slice(0, 5) + secondLetter
+        
+      } else {
+        row = row.slice(0, 1) + '*' + row.slice(2)
+        row = row.slice(0, 4) + '*' + row.slice(5)
+        row = row.slice(0, 2) + firstLetter + row.slice(3)
+        row = row.slice(0, 3) + secondLetter + row.slice(4)
+       
+      }
     }
+  
+    return [row, toTheEdges]
   }
 }
-
-dnaPrinting(10)
 
 // AT   -> 1   6    11   16
 // CG   -> 2   7    12   17
