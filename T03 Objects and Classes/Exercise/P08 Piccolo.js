b952310ca1847f8d0aea74ed2e15e8e22bb3bd1a
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-07-03 22:24:28 +0300

    This is the solution of the problem "Piccolo"

---
+++ T03 Objects and Classes/Exercise/P08 Piccolo.js
@@ -0,0 +1,50 @@
+function parking(array) {
+    let cars = []
+
+    for (let index in array) {
+        let commandAndCar = array[index].split(', ')
+        let currentCommand = commandAndCar[0]
+        let currentCar = commandAndCar[1]
+
+        if (currentCommand == 'IN' && !cars.includes(currentCar)) {
+            cars.push(currentCar)
+        } else if (currentCommand == 'OUT' && cars.includes(currentCar)) {
+            let carIndex = cars.indexOf(currentCar)
+            cars.splice(carIndex, 1)
+        }
+    }
+
+    if (cars.length == 0) {
+        console.log('Parking Lot is Empty')
+        return
+    } else {
+        cars.sort((a, b) => {
+            if (a > b) {
+                return 1
+            } else if (a < b) {
+                return -1
+            } else 0
+        })
+
+        cars.forEach( (currentCar) => console.log(currentCar))
+    }
+}
+
+// the input is an array. Every element of this array consist of command (IN or OUT) and a number of a car). When the command is IN - adding, OUT - removing 
+
+// parking(['IN, CA2844AA',
+//     'IN, CA1234TA',
+//     'OUT, CA2844AA',
+//     'IN, CA9999TT',
+//     'IN, CA2866HI',
+//     'OUT, CA1234TA',
+//     'IN, CA2844AA',
+//     'OUT, CA2866HI',
+//     'IN, CA9876HH',
+//     'IN, CA2822UU']
+// )
+
+// parking(['IN, CA2844AA',
+// 'IN, CA1234TA',
+// 'OUT, CA2844AA',
+// 'OUT, CA1234TA'])
