a5df47845f4ce87e48cef19db320a07ccff02a38
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-07-03 23:48:03 +0300

    This is the final solution of the problem "dictionary"

--- T03 Objects and Classes/Exercise/P09 Make a Dictionary.js
+++ T03 Objects and Classes/Exercise/P09 Make a Dictionary.js
@@ -22,12 +22,12 @@ function dictionary(array) {
 
 // The input is an array, consisted of JSON text. Every element must be parsed to object. And then every object m
 
-dictionary([
-    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
-    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
-    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
-    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
-    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
-]
-)
+// dictionary([
+//     '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+//     '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+//     '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+//     '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+//     '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+// ]
+// )
 
