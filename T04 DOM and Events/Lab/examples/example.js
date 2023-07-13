af85dfb7007bace6f2276fffe8517841769fed1d
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-07-13 17:27:29 +0300

    This is an example for click event

--- T04 DOM and Events/Lab/examples/example.js
+++ T04 DOM and Events/Lab/examples/example.js
@@ -1,2 +1,16 @@
-let element = document.getElementsByClassName('cat')[0]
+let button = document.getElementsByTagName('button')[0] 
+
+button.addEventListener('click', clickOnButton) 
+
+function clickOnButton(e) {
+    const currentTarget = e.currentTarget 
+    const textTarget = currentTarget.textContent
+
+    currentTarget.textContent = Number(textTarget) + 1 
+
+    button.removeEventListener('click', clickOnButton)
+}
+
+
+
 
