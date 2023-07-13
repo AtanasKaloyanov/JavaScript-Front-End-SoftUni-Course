28eaf9c56340575d40fe1a6cba339d2840d977a0
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2023-07-13 22:16:54 +0300

    This is the solution of the problem "List Of Items"

---
+++ T04 DOM and Events/Lab/P04  List Of Items/app.js
@@ -0,0 +1,18 @@
+function addItem() {
+    let newElement = document.createElement('li')
+    let thirdInputValue = document.getElementById('newItemText').value
+
+    newElement.textContent = thirdInputValue
+    document.getElementById('items').appendChild(newElement)
+}
+
+/*
+ <main>
+    <ul id="items">
+        <li>First</li>
+        <li>Second</li>
+    </ul>
+    <input type="text" id="newItemText" />
+    <input type="button" value="Add" onclick="addItem()">
+</main> 
+*/ 
