519906fa5fbc5554a995eb68eb785e5047f6508f
AtanasKaloyanov <atanas.kaloyanov@yahoo.com>
2025-01-06 18:02:04 +0200

    This is the solution of the problem "Hell's Kitchen".

--- T04 DOM and Events/More Exercices/P08. Hell's Kitchen/app.js
+++ T04 DOM and Events/More Exercices/P08. Hell's Kitchen/app.js
@@ -1,46 +1,73 @@
- function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+function solve() {
 
-   let textArea = document.getElementsByTagName('textarea')[0];
-   function onClick () {
-      //1. Parsing the input:
-      let text = textArea.value;
-      let infoArray = JSON.parse(text);
-      
-      // 2. Creating an asociative array and adding information:
-      let restaurantBook = {};
-      for (let i = 0; i < infoArray.length; i++) {
-         let restaurantWithWorkers = infoArray[i];
-         let restaurantWithWorkersArray = restaurantWithWorkers.split(' - ');
-         let restaurantName = restaurantWithWorkersArray[0];
+    //1. Getting the text from the textArea
+    let textArea = document.getElementsByTagName('textarea')[0];
+    let lines = textArea.value.split('\n');
+    let workersInfoByRestaurant = {};
 
-         // 2.1. 
-         if (!restaurantBook.hasOwnProperty(restaurantName)) {
-            restaurantBook[restaurantName] = [];
-         }
+    // 2. Adding the information to an associative array:
+    for (let i = 0; i < lines.length; i++) {
+        let text = lines[i];
+        let infoArray = JSON.parse(text);
 
-         let workersInfo = restaurantWithWorkersArray[1];
-         let workersInfoArray = workersInfo.split(', ');
-         
-         for (let i = 0; i < workersInfoArray.length; i++) {
-            let tuple = workersInfoArray[i];
-            let tupleArray = tuple.split(' ');
-            let workerName = tupleArray[0];
-            let workerSalary = Number(tupleArray[1]);
-            let newWorker = {};
-            newWorker.name = workerName;
-            newWorker.salary = workerSalary;
-            restaurantBook[restaurantName].push(newWorker);
-         }
-      }
-   
-       
-   }
- 
+        for (let i = 0; i < infoArray.length; i++) {
+            let restaurantWithWorkers = infoArray[i];
+            let restaurantWithWorkersArray = restaurantWithWorkers.split(' - ');
+            let restaurantName = restaurantWithWorkersArray[0];
+
+            if (!workersInfoByRestaurant.hasOwnProperty(restaurantName)) {
+                workersInfoByRestaurant
+                [restaurantName] = [];
+            }
+
+            let workersInfo = restaurantWithWorkersArray[1];
+            let workersInfoArray = workersInfo.split(', ');
+
+            for (let i = 0; i < workersInfoArray.length; i++) {
+                let tuple = workersInfoArray[i];
+                let tupleArray = tuple.split(' ');
+                let workerName = tupleArray[0];
+                let workerSalary = Number(tupleArray[1]);
+                let newWorker = {};
+                newWorker.name = workerName;
+                newWorker.salary = workerSalary;
+                workersInfoByRestaurant[restaurantName].push(newWorker);
+            }
+        }
+    }
+    // 3. Finding the best restaurant by sorting them by average salary:
+    let bestRestaurant = Object.entries(workersInfoByRestaurant)
+        .sort(([key1, value1], [key2, value2]) => {
+            let salaryAVG1 = avgSalary(value1);
+            let salaryAVG2 = avgSalary(value2);
+            return salaryAVG2 - salaryAVG1;
+        })[0];
+
+    // 4. Print variables preparing:
+    let bestRestaurantName = bestRestaurant[0];
+    let workers = bestRestaurant[1];
+    let bestAvgSalary = avgSalary(workers);
+    workers.sort((worker1, worker2) => worker2.salary - worker1.salary);
+    let bestWorkerSalary = workers[0].salary;
+    let workersFormatted = '';
+    workers.forEach((worker) => workersFormatted += `Name: ${worker.name} With Salary: ${worker.salary} `);
+
+    // 5. Getting the output elements:
+    let bestRestaurantElement = document.querySelector('#outputs #bestRestaurant p');
+    let bestWorkersElement = document.querySelector('#outputs #workers p')
+    bestRestaurantElement.textContent = `Name: ${bestRestaurantName} Average Salary: ${bestAvgSalary.toFixed(2)} Best Salary: ${bestWorkerSalary.toFixed(2)}`;
+    bestWorkersElement.textContent = `${workersFormatted}`;
+
+    // Calculating of the average salary:
+    function avgSalary(workers) {
+        let totalSum = workers.reduce((sum, person) => sum + person.salary, 0);
+        let number = workers.length;
+        return totalSum / number;
+    }
 }
+//}
 
 /*
-
 <body onload="solve()">
     <section class="restaurant-race-class">
         <div id="inputs">
@@ -65,5 +92,4 @@
     </section>
     <script src='app.js'></script>
 </body>
-
 */
